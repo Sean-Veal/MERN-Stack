29cf48a7e210f25fca10d547497cae82947e094b
Sean Veal <seanveal@Seans-MacBook-Pro.local>
2018-05-07 13:50:33 -0500

    feature: Facebook OAuth

--- server/lib/app.ts
+++ server/lib/app.ts
@@ -2,6 +2,8 @@ import * as express from "express";
 import * as bodyParser from "body-parser";
 import { Request, Response } from "express";
 import PassportConfig from './config/passport-config';
+import * as cookieSession from 'cookie-session';
+import * as passport from 'passport';
 import * as mongoose from 'mongoose';
 import * as keys from './config/keys';
 
@@ -19,6 +21,12 @@ class App {
       mongoose.connect(keys.mongoURI);
       this.app.use(bodyParser.json());
       this.app.use(bodyParser.urlencoded({ extended: false }));
+      this.app.use(cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+      }));
+      this.app.use(passport.initialize());
+      this.app.use(passport.session());
       PassportConfig.startPassportProcess();
   }
 
--- server/lib/config/passport-config.ts
+++ server/lib/config/passport-config.ts
@@ -1,19 +1,48 @@
     import * as passport from 'passport';
-    import * as GoogleStrategy from 'passport-google-oauth20';
+    import * as passportGoogle from 'passport-google-oauth20';
+    import * as passportFacebook from 'passport-facebook';
     import * as keys from './keys';
     import {User} from '../models/user';
     import {IUser} from '../interfaces/User';
+    import {Request, Response} from 'express';
 
     class PassportConfig {
         constructor() {}
 
+        private GoogleStrategy = passportGoogle.Strategy;
+        private FacebookStrategy = passportFacebook.Strategy;
+
         startPassportProcess() {
-            const strategyConfig: Object = {
+            const googleConfig: Object = {
                 clientID: keys.googleClientID,
                 clientSecret: keys.googleClientSecret,
                 callbackURL: '/auth/google/callback'
             };
-            passport.use(new GoogleStrategy.Strategy(strategyConfig, 
+            const facebookConfig: Object = {
+                clientID: keys.facebookAppId,
+                clientSecret: keys.facebookAppSecret,
+                callbackURL: '/auth/facebook/callback',
+                profileFields: ["name", "email", "link", "locale", "timezone"]
+            };
+            // This user is what's being pulled from the DB 
+            // That we just put in below
+            //Incoming Request with cookie
+            passport.serializeUser((user, done) => {
+                // user.id is the id from the mongo entry
+                done(null, user.id);
+            });
+
+            // Search DB for the id and turn it into a
+            // Mongose instance
+            passport.deserializeUser((id, done) => {
+                User.findById(id)
+                    .then((user) => {
+                        done(null, user);
+                    })
+            });
+
+            //Google OAuth Flow
+            passport.use(new this.GoogleStrategy(googleConfig, 
                 (accessToken, refreshToken, profile, done) => {
                     User.findOne({ googleId: profile.id })
                     .then((existingUser) => {
@@ -26,14 +55,32 @@
                         }
                     })
                 }));
+
+                //Facebook OAuth Flow
+                passport.use(new this.FacebookStrategy(facebookConfig,
+                (accessToken, refreshToken, profile, done) => {
+                    console.log('accessToken', accessToken);
+                    console.log('refreshToken', refreshToken);
+                    console.log('profile', profile);
+                    console.log('done', done);
+                }));
         }
 
             public getAuthRoutes(router) {
                 router.get('/auth/google', passport.authenticate('google', {
                     scope: ['profile', 'email']
                 }));
+
+                router.get('/auth/facebook', passport.authenticate('facebook', {
+
+                }));
                         
                 router.get('/auth/google/callback', passport.authenticate('google'));
+                router.get('/auth/facebook/callback', passport.authenticate('facebook'));
+
+                router.get('/api/current_user', (req: Request, res: Response) => {
+                    res.status(200).send(req.user);
+                });
             }
     }
 
