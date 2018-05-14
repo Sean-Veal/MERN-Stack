348f537cd8264b11f583fbdfc3bea9e3c29df2fd
Sean Veal <seanveal@Seans-MacBook-Pro.local>
2018-05-14 13:46:50 -0500

    fix: prod google strategy

--- server/lib/config/passport-config.ts
+++ server/lib/config/passport-config.ts
@@ -14,7 +14,8 @@
         private googleConfig: Object = {
             clientID: keys.googleClientID,
             clientSecret: keys.googleClientSecret,
-            callbackURL: '/auth/google/callback'
+            callbackURL: '/auth/google/callback',
+            proxy: true
         };
         private facebookConfig: Object = {
             clientID: keys.facebookAppId,
