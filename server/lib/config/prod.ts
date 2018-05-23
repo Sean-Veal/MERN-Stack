026afd50109afaf12ce5f5abd27cc6cc0f2ccb51
Sean Veal <seanveal@Seans-MacBook-Pro.local>
2018-05-23 15:42:15 -0500

    feature: started stripe config

--- server/lib/config/prod.ts
+++ server/lib/config/prod.ts
@@ -4,3 +4,5 @@ export const facebookAppId: string = process.env.FACEBOOK_APP_ID;
 export const facebookAppSecret: string = process.env.FACEBOOK_APP_SECRET;
 export const mongoURI: string = process.env.MONGO_URI;
 export const cookieKey: string = process.env.COOKIE_KEY;
+export const stripePublishableKey: string = process.env.STRIPE_PUBLISHABLE_KEY;
+export const stripeSecretKey: string = process.env.STRIPE_SECRET_KEY;
